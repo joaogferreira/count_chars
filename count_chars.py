7fc5babb69fdeffa05fce71aae6ce7413079b4cc
joaogferreira <jgmpof@ua.pt>
2020-12-16 17:46:17 +0000

    contadores check

--- count_chars.py
+++ count_chars.py
@@ -1,5 +1,6 @@
 import random
 import sys
+import math
 
 
 def exact_counter(sequence):
@@ -11,6 +12,35 @@ def exact_counter(sequence):
             results[char]=1
     return results
 
+def fixed_prob_counter(sequence):
+    results = {}
+    for char in sequence:
+        if char in results:
+            value = random.choices([0,1], weights = [7/8, 1/8])
+            results[char]+=value[0]
+        else:
+            value = random.choices([0,1], weights = [7/8, 1/8])
+            results[char]=value[0]
+    for r in results:
+        results[r]*=8
+    
+    return results
+
+def log_counter(sequence):
+    results = {}
+    for char in sequence:
+        if char in results:
+            value = random.choices([0,1], weights = [1-1/(math.sqrt(2)**results[char]), 1/(math.sqrt(2)**results[char])])
+            results[char]+=value[0]
+        else:
+            value = random.choices([0,1], weights = [0, 1]) #math.sqrt(2)**0  = 1
+            results[char]=value[0]
+    
+    for r in results:
+        results[r] = math.sqrt(2)**results[r]-1
+    
+
+    return results
 
 def main(size):
     name='joãoguilhermemendoncapimentadeoliveiraferreira'
@@ -20,8 +50,13 @@ def main(size):
         sequence += random.choice(name)
     
     exact = exact_counter(sequence)
-
     print(exact)
+
+    fixed = fixed_prob_counter(sequence)
+    print(fixed)
+
+    dec_counter = log_counter(sequence)
+    print(dec_counter)
 
 if __name__=='__main__':
     if len(sys.argv)!=2:
