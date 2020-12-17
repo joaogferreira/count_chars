bf89b5764626ee82cd4aff8c981761201396e9d6
joaogferreira <jgmpof@ua.pt>
2020-12-17 20:09:14 +0000

    exact vs fixed

--- count_chars.py
+++ count_chars.py
@@ -1,8 +1,17 @@
 import random
 import sys
 import math
+import logging
 
+def relative_error(counter1,counter2):
+    errors = []
+    for i in counter1:
+        errors.append(round((abs(counter1[i]-counter2[i])/counter1[i])*100,2))
+    avg = round(sum(errors)/len(errors),2)
+    max_error = max(errors)
+    min_error = min(errors)
 
+    return avg, max_error, min_error
 def exact_counter(sequence):
     results = {}
     for char in sequence:
@@ -50,15 +59,31 @@ def main(size):
         sequence += random.choice(name)
     
     exact = exact_counter(sequence)
+    print("Exact Counter:")
     print(exact)
+    print("\n")
 
+    print("Fixed Probability Counter:")
     fixed = fixed_prob_counter(sequence)
     print(fixed)
+    print("\n")
 
+    print("Decreasing Probability Logarithmic Counter:")
     dec_counter = log_counter(sequence)
     print(dec_counter)
+    print("\n")
+
+    print("*** Exact vs Fixed Counter ***")
+    avg_exact_fixed, max_exact_fixed, min_exact_fixed = relative_error(exact,fixed)
+    print("Average Relative Error: % s" %(str(avg_exact_fixed)+"%"))
+    print("Max Relative Error: % s" %(str(max_exact_fixed)+"%"))
+    print("Min Relative Error: % s" %(str(min_exact_fixed)+"%")) 
+    print("\n")
+    
+    print("*** Exact vs Logarithmic Counter ***")
+
 
 if __name__=='__main__':
     if len(sys.argv)!=2:
-        sys.exit("The size of the sequence must be passed as argument!")
+        sys.exit("Usage: python3 count_chars.py <sequence_size>")
     main(int(sys.argv[1]))
